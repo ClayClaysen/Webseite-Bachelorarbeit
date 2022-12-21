0792a5cd5a00154e7298da1686beebed4d3d1436
flobert auf macbook <Florian.Pusch@baierlach.de>
2022-12-21 17:53:06 +0100

    json sachen aber es klappt noch nicht und workpage

--- script/render.js
+++ script/render.js
@@ -1,24 +1,73 @@
-function renderContent() {
+let project_data;
+let about_me_data;
+let project_data_length;
+
+
+fetch('/script/data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        // console.log(json)
+        // console.log(json.about_me)
+        // console.log(json.projects)
+        // console.log(json.projects.length)
+
+        set_data(json.about_me, json.projects, json.projects.length)
+    });
+
+
+function set_data(about_me, projects, project_amount) {
+    about_me_data = about_me;
+    project_data = projects;
+    project_data_length = project_amount;
+
+    console.log(about_me_data);
+    console.log(project_data);
+    console.log(project_data_length);
+
+}
+
+
+function renderHome() {
+
+
+    var template_project = document.getElementById('template-project').innerHTML;
+    var template_about_me = document.getElementById('template-about_me').innerHTML;
+
+    var rendered_project = Mustache.render(template_project, project_data[0]);
+    var rendered_about_me = Mustache.render(template_about_me, about_me_data);
+
+    document.getElementById('projects-container').innerHTML = rendered_project;
+    document.getElementById('about_me-container').innerHTML = rendered_about_me;
+}
+
+
+function renderWork() {
 
     var template = document.getElementById('template').innerHTML;
-    var rendered = Mustache.render(template, projects[projects.length - 1]);
+    var rendered = Mustache.render(template, project_data);
     document.getElementById('projects-container').innerHTML = rendered;
 }
 
-var projects = [{
+var projects_old = [{
         projectname: "Luke",
         image: "/images/Aktivitätsmusik.png",
         projectdescription: "Chuck, Chuck, its' your cousin. Your cousin Marvin Berry, you know that new sound you're lookin for, well listen to this. Then how am I supposed to ever meet anybody."
     },
     {
-        projectname: "Luke",
+        projectname: "Jaggl",
         image: "/images/Aktivitätsmusik.png",
         projectdescription: "Chuck, Chuck, its' your cousin. Your cousin Marvin Berry, you know that new sound you're lookin for, well listen to this. Then how am I supposed to ever meet anybody."
     },
     {
-        projectname: "Malik Harris sein Vadder",
+        projectname: "Malik Harris sein  VAdder vadder Vadder",
         image: "/images/Aktivitätsmusik.png",
         projectdescription: "Chuck, Chuck, its' your cousin. Your cousin Marvin Berry, you know that new sound you're lookin for, well listen to this. Then how am I supposed to ever meet anybody."
     },
 
 ]
+
+var about_me_old = {
+    name: "Simon Klostermaier",
+    image: "/images/Aktivitätsmusik.png",
+    about_me_description: "Chuck, Chuck, its' your cousin. Your cousin Marvin Berry, you know that new sound you're lookin for, well listen to this. Then how am I supposed to ever meet anybody."
+}
